ddf2cf8b86fa1520cb05037a9575cec8eea93dea
samirabis <skynordp@gmail.com>
2021-02-25 12:50:34 +0000

    reverted change

--- app.js
+++ app.js
@@ -102,7 +102,7 @@ function displayMenuItems(menuItems) {
     <div class="item-info">
       <header>
         <h4>${item.title}</h4>
-        <h4 class="price">$${item.price}</h4>
+        <h4 class="price">${item.price}</h4>
       </header>
       <p class="item-text">
         ${item.desc}
